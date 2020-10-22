291058fac657e4a82c6890f0a8dc7af819309c95
Rodrigo Escobar <ocralo@gmail.com>
2020-10-22 01:06:56 -0500

    Add files via upload

---
+++ precache-manifest.c3ddbefae9a43377d1f6d24020b782f7.js
@@ -0,0 +1,86 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a1abe7ff7ae0a0c13fe828d654ea3ce2",
+    "url": "./index.html"
+  },
+  {
+    "revision": "4bd941ba6c927a6a167a",
+    "url": "./static/css/2.f524894f.chunk.css"
+  },
+  {
+    "revision": "3129195e5f8c8eb5624d",
+    "url": "./static/css/main.75439bba.chunk.css"
+  },
+  {
+    "revision": "4bd941ba6c927a6a167a",
+    "url": "./static/js/2.3142723e.chunk.js"
+  },
+  {
+    "revision": "0fc61ce0ee2d491f41ee7634c63d9b90",
+    "url": "./static/js/2.3142723e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "3129195e5f8c8eb5624d",
+    "url": "./static/js/main.ce7cfec8.chunk.js"
+  },
+  {
+    "revision": "9097b083ac141ff36372",
+    "url": "./static/js/runtime-main.1068b2ba.js"
+  },
+  {
+    "revision": "3d79515b3864f300b58b5b4240623dcc",
+    "url": "./static/media/chat.3d79515b.svg"
+  },
+  {
+    "revision": "41c409a95f681a3a6f116c3a488fd167",
+    "url": "./static/media/checklist.41c409a9.svg"
+  },
+  {
+    "revision": "a93a5733b7f42fafa37d05e4f7e8aeb0",
+    "url": "./static/media/comment.a93a5733.svg"
+  },
+  {
+    "revision": "8863cedd1f33f0bcc721117d607a8995",
+    "url": "./static/media/email.8863cedd.svg"
+  },
+  {
+    "revision": "700dba7024b41484270b6d1dc85d2f7d",
+    "url": "./static/media/facebook.700dba70.svg"
+  },
+  {
+    "revision": "c25a009af7f00c44fc81804fc202b110",
+    "url": "./static/media/filter.c25a009a.svg"
+  },
+  {
+    "revision": "5e98d379614c995a09c57cbd4bbcdd26",
+    "url": "./static/media/home.5e98d379.svg"
+  },
+  {
+    "revision": "11bd4edfffa561ce11decc1f2484bb95",
+    "url": "./static/media/left-arrow.11bd4edf.svg"
+  },
+  {
+    "revision": "5a31e3a9578d3173a4e8c57e084ce7a0",
+    "url": "./static/media/list.5a31e3a9.svg"
+  },
+  {
+    "revision": "4a29a7c65df9233f5eac6aeca4c73314",
+    "url": "./static/media/right-arrow.4a29a7c6.svg"
+  },
+  {
+    "revision": "9874dcc860792c6dae7cd104a8aeb1b3",
+    "url": "./static/media/telephone.9874dcc8.svg"
+  },
+  {
+    "revision": "e1195dfa452f72e90c08b456c3541f3b",
+    "url": "./static/media/telephoneCircle.e1195dfa.svg"
+  },
+  {
+    "revision": "007be26da3cc1bb21d9a29e0312bd245",
+    "url": "./static/media/user.007be26d.svg"
+  },
+  {
+    "revision": "47c77bf133116ff76838809ae5d2c83a",
+    "url": "./static/media/whatsapp.47c77bf1.svg"
+  }
+]);
